a2220ba275d8885b2fcb38892f74b969ba1b4c08
NinaWie <wnina@student.ethz.ch>
2022-04-08 22:40:28 +0200

    add wasserstein distance and align evaluation functions

--- fill_matrix.py
+++ fill_matrix.py
@@ -15,20 +15,26 @@ import matplotlib
 from sklearn.metrics import mean_squared_error
 
 
-def calculate_reciprocal_rank(df, k=10, distance_column="distance"):
+def calculate_reciprocal_rank(df, k=10, return_reciprocal=False, distance_column="distance"):
     # TODO: computing top-k-accuracy with this function vs the other function - results don't match
 
-    df["rank"] = df.groupby(by=["p_duration", "p_user_id", "u_duration", "p_filename", "u_filename"])[
+    df["rank"] = df.groupby(by=["p_duration", "u_user_id", "u_duration", "p_filename", "u_filename"])[
         distance_column
     ].rank()
-    df["reciprocal_rank"] = 1 / df["rank"]
     df_rank_filtered = df[df["same_user"]]
 
-    # for top 10
-    # df_rank_filtered["topk"] = (df_rank_filtered["rank"] <= k).astype(int)
-    # matrix_elements = df_rank_filtered.groupby(by=["p_duration", "u_duration"])["topk"].agg(["mean", "std"])
+    if return_reciprocal:
+        df_rank_filtered["reciprocal_rank"] = 1 / df_rank_filtered["rank"]
+        matrix_elements = df_rank_filtered.groupby(by=["p_duration", "u_duration"])["reciprocal_rank"].agg(
+            ["mean", "std"]
+        )
+        mean_matrix = matrix_elements["mean"].unstack(level=-1, fill_value=None)
+        std_matrix = matrix_elements["std"].unstack(level=-1, fill_value=None)
+        return mean_matrix, std_matrix
 
-    matrix_elements = df_rank_filtered.groupby(by=["p_duration", "u_duration"])["reciprocal_rank"].agg(["mean", "std"])
+    # for top k
+    df_rank_filtered["topk"] = (df_rank_filtered["rank"] <= k).astype(int)
+    matrix_elements = df_rank_filtered.groupby(by=["p_duration", "u_duration"])["topk"].agg(["mean", "std"])
 
     mean_matrix = matrix_elements["mean"].unstack(level=-1, fill_value=None) * 100
     std_matrix = matrix_elements["std"].unstack(level=-1, fill_value=None) * 100
@@ -41,7 +47,7 @@ def calculate_topk_accuracy(df, k, distance_column="distance"):
     # https://stackoverflow.com/a/71130117/16232216
 
     min_row_ix_by_group = (
-        df.groupby(by=["p_duration", "p_user_id", "u_duration", "p_filename"])[distance_column]
+        df.groupby(by=["p_duration", "u_user_id", "u_duration", "p_filename", "u_filename"])[distance_column]
         .nsmallest(k)
         .index.get_level_values(-1)
     )
@@ -50,7 +56,7 @@ def calculate_topk_accuracy(df, k, distance_column="distance"):
 
     # Group by group and calculate if a single group had a correct guess (yes or no). Column same user is boolean
     correct_guess_by_group = (
-        min_by_group.groupby(by=["p_duration", "p_user_id", "u_duration", "p_filename"])["same_user"]
+        min_by_group.groupby(by=["p_duration", "u_user_id", "u_duration", "p_filename", "u_filename"])["same_user"]
         .sum()
         .reset_index()
     )
@@ -71,11 +77,11 @@ def calculate_topk_accuracy(df, k, distance_column="distance"):
 if __name__ == "__main__":
 
     STUDY = "gc1"
-    DIST_COL = "kldiv_in_degree"
 
+    os.makedirs("outputs", exist_ok=True)
     engine = get_engine(DBLOGIN_FILE=os.path.join("dblogin.json"))
     print("download distances")
-    distances_query = f"SELECT * FROM {STUDY}.distance"
+    distances_query = f"SELECT * FROM {STUDY}.distance"  #  WHERE p_duration>16 and u_duration<16"  # for testing:
     feature_cross_product_df = pd.read_sql(distances_query, con=engine)
 
     # calculate same_user_flag (important for topk acc)
@@ -83,21 +89,35 @@ if __name__ == "__main__":
         feature_cross_product_df["p_user_id"] == feature_cross_product_df["u_user_id"]
     )
 
+    DIST_COL = "kldiv_in_degree"
     mean_matrix, std_matrix = calculate_topk_accuracy(
         feature_cross_product_df, k=10, distance_column=DIST_COL
     )  # uses the column distance
 
-    print("\nMEAN")
+    print("Output original k accuracy function:")
     print(mean_matrix)
-    # print("\nSTD")
-    # print(std_matrix)
 
-    print("RECIPROCAL")
-    mean_matrix, std_matrix = calculate_reciprocal_rank(
-        feature_cross_product_df, k=10, distance_column=DIST_COL
-    )  # uses the column distance
+    mean_matrix, std_matrix = calculate_reciprocal_rank(feature_cross_product_df, k=10, distance_column=DIST_COL)
 
-    print("\nMEAN")
+    print("Output new function(based on rank):")
     print(mean_matrix)
-    # print("\nSTD")
-    # print(std_matrix)
+
+    # Run on all dist types
+    for dist_col in [
+        "kldiv_in_degree",
+        "mse_in_degree",
+        "wasserstein_in_degree",
+        "kldiv_shortest_path",
+        "mse_shortest_path",
+        "wasserstein_shortest_path",
+        "kldiv_out_degree",
+        "mse_out_degree",
+        "wasserstein_out_degree",
+        "kldiv_centrality",
+        "mse_centrality",
+        "wasserstein_centrality",
+    ]:
+        mean_matrix, std_matrix = calculate_reciprocal_rank(feature_cross_product_df, k=10, distance_column=dist_col)
+        mean_matrix.to_csv(os.path.join("outputs", "mean_" + dist_col + ".csv"))
+        std_matrix.to_csv(os.path.join("outputs", "std_" + dist_col + ".csv"))
+
--- similarity.py
+++ similarity.py
@@ -2,27 +2,33 @@ import numpy as np
 import pandas as pd
 import time
 from scipy.special import rel_entr
+from scipy.stats import wasserstein_distance
 
 from utils import get_engine
 
 
 def kl_div(dist1, dist2):
-    return rel_entr(dist1, dist2)
+    return np.sum(rel_entr(dist1, dist2), axis=1)
 
 
 def mse(dist1, dist2):
-    return (dist1 - dist2) ** 2
+    return np.sum((dist1 - dist2) ** 2, axis=1)
+
+
+def wasserstein(dist1, dist2):
+    div = []
+    for i in range(len(dist1)):
+        div.append(wasserstein_distance(dist1[i], dist2[i]))
+    return div
 
 
 def get_div_array(dist1, dist2, metric="kldiv"):
+    """dist1, dist2: 2D numpy arrays of shape (nr_samples, distribution_size)"""
     # get row-wise divergence for an array
-    metric_dict = {"kldiv": kl_div, "mse": mse}
+    metric_dict = {"kldiv": kl_div, "mse": mse, "wasserstein": wasserstein}
     metric_fun = metric_dict[metric]
-    elementwise_div = metric_fun(dist1, dist2)
-    if len(dist1.shape) == 1:
-        return np.sum(elementwise_div)
-    else:
-        return np.sum(elementwise_div, axis=1)
+    div = metric_fun(dist1, dist2)
+    return div
 
 
 def get_similarity(df, columns_for_similarity, metric="kldiv"):
@@ -43,7 +49,7 @@ def get_similarity(df, columns_for_similarity, metric="kldiv"):
         print(feats_user.shape, feats_pool.shape)
 
         # compute divergence
-        for metric in ["kldiv", "mse"]:
+        for metric in ["kldiv", "mse", "wasserstein"]:
             out_col_name = f"{metric}_{col[:-6]}"
             df[out_col_name] = get_div_array(feats_pool, feats_user, metric=metric)
 
@@ -63,8 +69,9 @@ if __name__ == "__main__":
     tic = time.time()
     df = pd.read_sql(f"SELECT * FROM {study}.dur_features_cross_join", engine)
     print("loaded data...")
-    df_out = get_similarity(df, ["in_degree_feats", "shortest_path_feats"])
-    print("similarity computed...")
-    df_out.to_sql("distance", engine, study, if_exists="replace")
 
+    df_out = get_similarity(df, ["in_degree_feats", "shortest_path_feats", "out_degree_feats", "centrality_feats"])
+    print("similarity computed...")
+
+    df_out.to_sql("distance", engine, study, if_exists="replace")
     print("written to db, time for processing", time.time() - tic)
