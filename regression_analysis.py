b7a15f9f2635b3fc862cc86d1504b6a59fe4237d
Henry Martin <martinhe@ethz.ch>
2022-08-12 14:48:52 +0200

    BUG: Added todo for missing reporting of results

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,11 +1,16 @@
+"""Regression analysis to evaluate the dependence of pool- and test-user tracking duration on the
+matching performance (see table 1 in paper)"""
+
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+
 study = "gc1"
 out_df = {}
 p_value_dict = {}
 row_dict_list = []
+
 for k in [0, 5, 10]:
     res = pd.read_csv(f"./outputs/acc_k{k}/mean_mse_combined.csv", index_col="p_duration")
     res_arr = np.array(res)
@@ -19,15 +24,21 @@ for k in [0, 5, 10]:
 
     temp_list = reg.coef_.tolist() + [reg.intercept_]
 
-    row_dict_list.append("Coefficient pool duration": )
+    # todo: results of table 1 are calculated but not stored
+    row_dict_list.append("Coefficient pool duration": 1)
 
 
 
     X2 = sm.add_constant(X)
     est = sm.OLS(Y, X2)
     est2 = est.fit()
+
     p_values = est2.pvalues.tolist()
     p_value_dict[k] = p_values
 
+
+
 df = pd.DataFrame(out_df).rename(columns={0: "MRR", 1: "1-Accuracy", 5: "5-Accuracy", 10:"10-Accuracy"},
                             index={0:"Coefficient pool duration", 1: "Coefficient test duration", 2: "Coefficient of absolute difference between pool and test duration", 3: "Intercept"}).swapaxes(1,0)
+
+
